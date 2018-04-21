5449cb521bb480969777e134e666c87da6f53698
NilsJac <35526450+NilsJac@users.noreply.github.com>
2018-04-21 16:31:54 +0200

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;
+/*import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -36,6 +36,61 @@ public class Test {
             e.printStackTrace();
         }
     }
+	}*/
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class Test {
+	public static void main(String[] args){
+		boolean found =false;
+		String mot="cheval";
+		
+	try {
+		File fileDir = new File("images/ListeMots.txt");
+			
+		BufferedReader in = new BufferedReader(
+		   new InputStreamReader(
+                      new FileInputStream(fileDir), "UTF16"));
+		        
+		String str;
+		      
+		while ((str = in.readLine()) != null) {
+		
+			
+            System.out.println(str);
+           if (str.equals(mot)) {
+        	   		found=true;
+           }
+		}
+		        
+                in.close();
+                
+                if(found)
+                {
+                	 System.out.println("le mot existe ");
+                }
+                else {
+                	System.out.println("le mot n'existe pas ");
+                }
+	    } 
+	    catch (UnsupportedEncodingException e) 
+	    {
+			System.out.println(e.getMessage());
+	    } 
+	    catch (IOException e) 
+	    {
+			System.out.println(e.getMessage());
+	    }
+	    catch (Exception e)
+	    {
+			System.out.println(e.getMessage());
+	    }
 	}
+}
 
 

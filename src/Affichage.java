525d43618a5d136a2a744a61a2fd53b046d97292
nikolino11 <38403568+nikolino11@users.noreply.github.com>
2018-04-23 14:36:45 +0200

    Update Affichage.java

--- src/Affichage.java
+++ src/Affichage.java
@@ -1,5 +1,3 @@
-package Affichage;
-
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
@@ -23,14 +21,14 @@ import javax.swing.JPanel;
 
 public class Affichage {
 
-private static JPanel plateau,background,saturne;
+private static JPanel plateau,background,saturne,score,barrescore,barrejoueur,pioche,menu;
 
 public Affichage() {
 	
 	JFrame fenetre = new JFrame();
 	BufferedImage myPicture = null;
     
-    fenetre.setSize(1250, 800);
+    fenetre.setSize(1351, 800);
     fenetre.setResizable(false);
     
     
@@ -38,6 +36,7 @@ public Affichage() {
     saturne = new JPanel();
 	//myPannel.setBackground(Color.GREEN);
     saturne.setLocation(0, 0);
+    saturne.setOpaque(false);
     saturne.setLayout(null);
 	
 	
@@ -53,7 +52,114 @@ public Affichage() {
 	   JLabel picLabel2 = new JLabel(new ImageIcon(myPicture));
 	    saturne.add(picLabel2);
 	    fenetre.add(saturne);
+	    
+	    
+
+	    score = new JPanel();
+		//myPannel.setBackground(Color.GREEN);
+	    score.setLocation(0, 0);
+	    score.setOpaque(false);
+	    score.setLayout(null);
+		
+		
+			try {
+				System.out.println("****");
+				myPicture = ImageIO.read(new File("C:\\Users\\Nicolas\\eclipse-workspace\\affichage\\score_txt_score.png"));
+			} catch (IOException e) {
+				System.out.println("****");
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("****");
+		   JLabel picLabel3 = new JLabel(new ImageIcon(myPicture));
+		   score.add(picLabel3);
+		    fenetre.add(score);
     
+		    
+		    barrescore = new JPanel();
+			//myPannel.setBackground(Color.GREEN);
+		    barrescore.setLocation(0, 0);
+		    barrescore.setOpaque(false);
+		    barrescore.setLayout(null);
+			
+			
+				try {
+					System.out.println("****");
+					myPicture = ImageIO.read(new File("C:\\Users\\Nicolas\\eclipse-workspace\\affichage\\score_bar.png"));
+				} catch (IOException e) {
+					System.out.println("****");
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("****");
+			   JLabel picLabel4 = new JLabel(new ImageIcon(myPicture));
+			   barrescore.add(picLabel4);
+			    fenetre.add(barrescore);
+			    
+			    
+			    barrejoueur = new JPanel();
+				//myPannel.setBackground(Color.GREEN);
+			    barrejoueur.setLocation(0, 0);
+			    barrejoueur.setOpaque(false);
+			    barrejoueur.setLayout(null);
+				
+				
+					try {
+						System.out.println("****");
+						myPicture = ImageIO.read(new File("C:\\Users\\Nicolas\\eclipse-workspace\\affichage\\game_bar.png"));
+					} catch (IOException e) {
+						System.out.println("****");
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println("****");
+				   JLabel picLabel5 = new JLabel(new ImageIcon(myPicture));
+				   barrejoueur.add(picLabel5);
+				    fenetre.add(barrejoueur);
+				    
+				   pioche = new JPanel();
+				   //myPannel.setBackground(Color.GREEN);
+				   pioche.setLocation(0, 0);
+				   pioche.setOpaque(false);
+				   pioche.setLayout(null);
+					
+					
+					try {
+						System.out.println("****");
+						myPicture = ImageIO.read(new File("C:\\Users\\Nicolas\\eclipse-workspace\\affichage\\game_pick_empty.png"));
+					} catch (IOException e) {
+						System.out.println("****");
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println("****");
+				   JLabel picLabel6 = new JLabel(new ImageIcon(myPicture));
+				   pioche.add(picLabel6);
+				   fenetre.add(pioche);
+				   
+				   
+				   
+				   menu = new JPanel();
+					//myPannel.setBackground(Color.GREEN);
+				   menu.setLocation(0, 0);
+				   menu.setOpaque(false);
+				   menu.setLayout(null);
+					
+					
+						try {
+							System.out.println("****");
+							myPicture = ImageIO.read(new File("C:\\Users\\Nicolas\\eclipse-workspace\\affichage\\game_pick_empty.png"));
+						} catch (IOException e) {
+							System.out.println("****");
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						System.out.println("****");
+					   JLabel picLabel7 = new JLabel(new ImageIcon(myPicture));
+					   menu.add(picLabel7);
+					    fenetre.add(menu);
+			    
+		    
     
 	    
 	plateau = new JPanel();
@@ -108,10 +214,19 @@ public Affichage() {
 	    picLabel.setBounds(0, 0, 1367, 769);
 	    picLabel1.setBounds(0, 0, 731, 730);
 	    picLabel2.setBounds(0, 0, 64, 39);
+	    picLabel3.setBounds(0, 0, 110, 26);
+	    picLabel4.setBounds(0, 0, 351, 4);
+	    picLabel5.setBounds(0, 0, 463, 5);
+	    picLabel6.setBounds(0, 0, 64, 86);
+	    picLabel7.setBounds(0, 0, 104, 41);
 	    background.setBounds(0, 0, 1367, 769);
 	    plateau.setBounds(20, 20, 731, 730);
-	    saturne.setBounds(770, 180, 64, 39);
-	    
+	    saturne.setBounds(770, 240, 64, 39);
+	    score.setBounds(840, 250, 110, 26);
+	    barrescore.setBounds(1000, 261, 351, 4);
+	    barrejoueur.setBounds(761, 745, 463, 5);
+	    pioche.setBounds(1220, 664, 64, 86);
+	    menu.setBounds(1247, 20, 104, 41);
 	    
 	    //myPannel.setLocation(20, 20);
 	    fenetre.setVisible(true);

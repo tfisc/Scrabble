24cea80a44ac78146451cd538a07e60b84b90594
Devanel <38227269+Devanel@users.noreply.github.com>
2018-04-23 17:54:08 +0200

    Update Plateau.java

--- src/Plateau.java
+++ src/Plateau.java
@@ -123,7 +123,157 @@ public class Plateau {
 		return rep;
 	}
 
-	
+	//tout le long pour les vérifications j'estime qu'une case vide a un bonus de 0
+		public boolean caseValide(int i, int j) { //j'estime que la case sélectionnée par le jour est donnée sous ses coordonnées i et j
+			
+			//si c'est la case de départ et qui est vide
+			if(i==8 && j==8 && bonus[i][j]!=0) {
+				return true;
+			}
+			
+			//si la case est déjà prise
+			if(bonus[i][j]==0) {
+				return false;
+			}
+			
+			//si case vide et sans rien autour
+			if(bonus[i][j]!=0 && bonus[i+1][j]!=0 && bonus[i-1][j]!=0 && bonus[i][j-1]!=0 && bonus[i][j+1]!=0) {
+				return false;
+			}
+			
+			//si case vide et au moins une lettre autour
+			if(bonus[i][j]!=0 && (bonus[i+1][j]==0 || bonus[i-1][j]==0 || bonus[i][j-1]==0 || bonus[i][j+1]==0)) {
+				return true;
+			}
+		}
+		
+		public boolean placeDisponible(int nbLettres, int i, int j, int d) { //j'estime que la méthode récupère le nombre de lettres sélectionnées par le joueur et les coordonnées de la case de départ sélectionnée et la direction (0 pour en bas, 1 pour à droite); 			
+			int caseR=0;
+			int curseur=0;
+			
+			//si la direction sélectionnée est vers le bas
+			if(d==0) {
+				//initialisation du curseur au niveau de la ligne de la case de départ sélectionnée
+				curseur=i;
+				while(curseur!=15) {
+					if(bonus[curseur][j]!=0) {
+						caseR ++;
+					}
+					curseur ++;
+				}
+				//si les cases disponibles sont supérieures ou égales aux lettres sélectionnées alors retournent vraies
+				if(caseR>=nbLettres) {
+					return true;
+				}
+			}
+			
+			//si la direction sélectionnée est vers la droite
+			if(d==1) {
+				curseur=j;
+				while(curseur!=15) {
+					if(bonus[i][curseur]!=0) {
+						caseR ++;
+					}
+					if(caseR>=nbLettres) {
+						return true;
+					}
+					curseur ++;
+				}
+			}
+			return false;
+		}
+		
+		public boolean motValide(String mot) {
+			
+			if(Test(mot) == true) {
+				return true;
+			}
+			
+			else {
+				return false;
+			}
+		}
+		
+		
+		public boolean motAtourValide(int i, int j) {
+		//on suppose qu'une case déjà utilisée par une lettre sur la plateau a un bonus égal à 0	
+			String motVerif = "";
+			int curseur1=0;
+			
+			//case de départ du mot joué (qu'on va appeler case d'origine) a une lettre voisine à gauche et pas à droite ou aussi à droite
+			if (bonus[i][j-1]!=0) {
+				
+				//initialisation du curseur au début du mot voisin de gauche et du mot à vérifier à vide
+				motVerif="";
+				curseur1=j;
+				while(bonus[i][curseur1]!=0) {
+					curseur1=curseur1-1;
+				}
+				
+				//parcours du mot jusqu'à la fin et construction du mot à vérifier
+				while(bonus[i][curseur1]!=0) {
+					motVerif = motVerif + lettreCase(i,curseur1); //lettreCase est une méthode qui permet de retourner la lettre posée sur la case sous forme de String
+					curseur1=curseur1+1;
+				}
+				
+				//si mot à vérifier faux, retourne faux
+				if(Test(motVerif)==false) {
+					return false;
+				}
+			}
+			
+			//case d'origine a une lettre voisine à droite et pas à gauche
+			if(bonus[i][j-1]==0 && bonus[i][j+1]!=0) {
+				motVerif="";
+				curseur1=j;
+				
+				//parcours du mot jusqu'à la fin et construction du mot à vérifier
+				while(bonus[i][curseur1]!=0) {
+					motVerif=motVerif + lettreCase(i,curseur1);
+					curseur1 ++;
+				}
+				
+				//si mot à vérifier faux, retourne faux
+				if(Test(motVerif)=false) {
+					return false;
+				}
+			}
+			
+			//case d'origine a une lettre voisine en haut et pas en bas ou aussi en bas
+			
+			if(bonus[i-1][j]!=0) {
+				motVerif="";
+				curseur1=i;
+				while(bonus[curseur1][j]!=0) {
+					curseur1 --;
+				}
+				
+				while(bonus[curseur1][j]!=0) {
+					motVerif=motVerif + lettreCase(curseur1,j);
+					curseur1 ++;
+				}
+				if(Test(motVerif)==false) {
+					return false;
+				}
+			}
+			
+			if(bonus[i+1][j]!=0) {
+				motVerif="";
+				curseur1=i;
+				
+				while(bonus[curseur1][j]!=0) {
+					motVerif=motVerif+lettreCase(cuseur1,j);
+					curseur1 ++;
+				}
+				if(Test(motVerif)==false) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		public boolean poseMotValide() {
+		}
 	
 	
 	

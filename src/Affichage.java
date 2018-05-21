7d885ea8eb288573f82a6d258d72f77cf8cf0c89
Thibault Fischer <thibault.fischer0@gmail.com>
2018-05-21 11:22:56 +0200

    gestion clique

--- src/Affichage.java
+++ src/Affichage.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -22,7 +23,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Affichage extends JPanel{
+public class Affichage extends JPanel implements MouseListener{
 	
 	private ImageIcon plateau;
 	private Image plateauimg;
@@ -78,6 +79,7 @@ public Affichage() {
 	pick=new ImageIcon(getClass().getResource("/images/game_pick_empty.png"));
 	pickimg=pick.getImage();
 
+
 	  }   
 
 
@@ -91,8 +93,10 @@ public void paintComponent(Graphics g) {
 	g.drawImage(scorebarimg,1020,250, null);
 	g.drawImage(gamebarimg,800,720, null);
 	g.drawImage(pickimg,1200,650, null);
+	this.addMouseListener(this);
 	
 }
+
 public static void main(String[] args){
     
     Affichage test=new Affichage();
@@ -105,222 +109,62 @@ public static void main(String[] args){
     
     fenetre.setContentPane(test);
     fenetre.setVisible(true);
+    
+   
    
     
   
 
 
 }
+
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	int xclick;
+	int yclick;
+	xclick=e.getX();
+	yclick=e.getY();
+	if(xclick<=730 && yclick<=730) {
+		xclick=xclick/49;
+		yclick=yclick/49;
+		System.out.println("vous avez cliqué sur la case "+xclick+","+yclick);
+	}
+	else {
+		System.out.println("vous n'avez pas cliqué sur le plateau");
+		
+	}
+	
+}
+
+
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
 
 
 
-/*private static JPanel plateau,background,saturne,score,barrescore,barrejoueur,pioche,menu;
-
-public Affichage() {
-	
-	JFrame fenetre = new JFrame();
-	BufferedImage myPicture = null;
-    
-    fenetre.setSize(1351, 800);
-    fenetre.setResizable(false);
-    
-    
-    
-    saturne = new JPanel();
-	//myPannel.setBackground(Color.GREEN);
-    saturne.setLocation(0, 0);
-    saturne.setOpaque(false);
-    saturne.setLayout(null);
-	
-	
-		try {
-			System.out.println("****");
-			myPicture = ImageIO.read(new File("images/score_planet.png"));
-		} catch (IOException e) {
-			System.out.println("****");
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("****");
-	   JLabel picLabel2 = new JLabel(new ImageIcon(myPicture));
-	    saturne.add(picLabel2);
-	    fenetre.add(saturne);
-	    
-	    
-
-	    score = new JPanel();
-		//myPannel.setBackground(Color.GREEN);
-	    score.setLocation(0, 0);
-	    score.setOpaque(false);
-	    score.setLayout(null);
-		
-		
-			try {
-				System.out.println("****");
-				myPicture = ImageIO.read(new File("images/score_txt_score.png"));
-			} catch (IOException e) {
-				System.out.println("****");
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("****");
-		   JLabel picLabel3 = new JLabel(new ImageIcon(myPicture));
-		   score.add(picLabel3);
-		    fenetre.add(score);
-    
-		    
-		    barrescore = new JPanel();
-			//myPannel.setBackground(Color.GREEN);
-		    barrescore.setLocation(0, 0);
-		    barrescore.setOpaque(false);
-		    barrescore.setLayout(null);
-			
-			
-				try {
-					System.out.println("****");
-					myPicture = ImageIO.read(new File("images/score_bar.png"));
-				} catch (IOException e) {
-					System.out.println("****");
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println("****");
-			   JLabel picLabel4 = new JLabel(new ImageIcon(myPicture));
-			   barrescore.add(picLabel4);
-			    fenetre.add(barrescore);
-			    
-			    
-			    barrejoueur = new JPanel();
-				//myPannel.setBackground(Color.GREEN);
-			    barrejoueur.setLocation(0, 0);
-			    barrejoueur.setOpaque(false);
-			    barrejoueur.setLayout(null);
-				
-				
-					try {
-						System.out.println("****");
-						myPicture = ImageIO.read(new File("images/game_bar.png"));
-					} catch (IOException e) {
-						System.out.println("****");
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					System.out.println("****");
-				   JLabel picLabel5 = new JLabel(new ImageIcon(myPicture));
-				   barrejoueur.add(picLabel5);
-				    fenetre.add(barrejoueur);
-				    
-				   pioche = new JPanel();
-				   //myPannel.setBackground(Color.GREEN);
-				   pioche.setLocation(0, 0);
-				   pioche.setOpaque(false);
-				   pioche.setLayout(null);
-					
-					
-					try {
-						System.out.println("****");
-						myPicture = ImageIO.read(new File("images/game_pick_empty.png"));
-					} catch (IOException e) {
-						System.out.println("****");
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					System.out.println("****");
-				   JLabel picLabel6 = new JLabel(new ImageIcon(myPicture));
-				   pioche.add(picLabel6);
-				   fenetre.add(pioche);
-				   
-				   
-				   
-				   menu = new JPanel();
-					//myPannel.setBackground(Color.GREEN);
-				   menu.setLocation(0, 0);
-				   menu.setOpaque(false);
-				   menu.setLayout(null);
-					
-					
-						try {
-							System.out.println("****");
-							myPicture = ImageIO.read(new File("images/menu.png"));
-						} catch (IOException e) {
-							System.out.println("****");
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						System.out.println("****");
-					   JLabel picLabel7 = new JLabel(new ImageIcon(myPicture));
-					   menu.add(picLabel7);
-					    fenetre.add(menu);
-			    
-		    
-    
-	    
-	plateau = new JPanel();
-	//myPannel.setBackground(Color.GREEN);
-	plateau.setLocation(0, 0);
-	plateau.setLayout(null);
-	
-	
-		try {
-			System.out.println("****");
-			myPicture = ImageIO.read(new File("images/board.png"));
-		} catch (IOException e) {
-			System.out.println("****");
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("****");
-	    JLabel picLabel1 = new JLabel(new ImageIcon(myPicture));
-	    plateau.add(picLabel1);
-	    fenetre.add(plateau);
-	    
-	    
-	    
-	    
-		    
-		    
-		    
-		    
-		    background = new JPanel();
-			
-		    background.setLocation(0, 0);
-		    background.setLayout(null);
-			
-			
-				try {
-					System.out.println("****");
-					myPicture = ImageIO.read(new File("images/background.png"));
-				} catch (IOException e) {
-					System.out.println("****");
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println("****");
-						
-				
-				
-				JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-			    background.add(picLabel);
-			    fenetre.add(background);
-	    
-	    
-	    picLabel.setBounds(0, 0, 1367, 769);
-	    picLabel1.setBounds(0, 0, 731, 730);
-	    picLabel2.setBounds(0, 0, 64, 39);
-	    picLabel3.setBounds(0, 0, 110, 26);
-	    picLabel4.setBounds(0, 0, 351, 4);
-	    picLabel5.setBounds(0, 0, 463, 5);
-	    picLabel6.setBounds(0, 0, 64, 86);
-	    picLabel7.setBounds(0, 0, 104, 41);
-	    background.setBounds(0, 0, 1367, 769);
-	    plateau.setBounds(20, 20, 731, 730);
-	    saturne.setBounds(770, 240, 64, 39);
-	    score.setBounds(840, 250, 110, 26);
-	    barrescore.setBounds(1000, 261, 351, 4);
-	    barrejoueur.setBounds(761, 745, 463, 5);
-	    pioche.setBounds(1220, 664, 64, 86);
-	    menu.setBounds(1247, 20, 104, 41);
-	    
-	    //myPannel.setLocation(20, 20);
-	    fenetre.setVisible(true);*/
